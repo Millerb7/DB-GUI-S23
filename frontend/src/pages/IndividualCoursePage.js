f9ee0cfb7340cb32ef81228729a3ba43ab3563c9
CarsonRPittman <60508384+CarsonRPittman@users.noreply.github.com>
2023-04-22 14:01:34 -0500

    Indiv Course Page

--- frontend/src/pages/IndividualCoursePage.js
+++ frontend/src/pages/IndividualCoursePage.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { getCourseById } from "src/api/coursePageApi";
-import Page from '../components/Page'
+import { getUserAssignments } from "src/api/AssignmentApi";
+import { AssignmentList } from "src/components/AssignmentList";
+import { Button } from "@mui/material";
 
 export const IndividualCoursePage = () => {
 
-    const [course, setCourse] = useState(undefined);
+    const [course, setCourse] = useState([]);
+    const [assignments, setAssignments] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         getCourseById(course).then(x => setCourse(x));
+        // getUserAssignments(userid).then(x => setAssignments(x))
     }, []);
 
     return<>
-        <h1>hello</h1>
+        <Button type="button" onClick={()=>{
+        }}>Add Assignment</Button>
+
+        <h2 align="center">Missing Assignments</h2>
+        {/* <AssignmentList assignments={assignments}/> */}
+
+        <h2 align="center">Current Assignments</h2>
     </>
 }
