fe3bbb2a44724823af54e5b50d3bcfd50f77e236
Kayla S <104600238+kssimone@users.noreply.github.com>
2023-04-24 14:28:20 -0500

    removed course_number from & fixed dashboard path

--- frontend/src/layouts/dashboard/SidebarConfig.js
+++ frontend/src/layouts/dashboard/SidebarConfig.js
@@ -8,7 +8,7 @@ const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 const sidebarConfig = [
   {
     title: 'Dashboard',
-    path: '/dashboard/app',
+    path: '',
     icon: getIcon('eva:pie-chart-2-fill')
   },
   {
