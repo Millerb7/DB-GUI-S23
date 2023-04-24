691e1e98986532a5c6c18486d1d69fefb2504977
amandavmonaco <avmonaco@smu.edu>
2023-04-24 15:34:41 -0500

    styling home page

--- frontend/src/pages/DashboardApp.js
+++ frontend/src/pages/DashboardApp.js
@@ -7,11 +7,11 @@ import { Home } from './Home';
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
+
   return (
     <Page title="Dashboard | Minimal-UI">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
-          <Typography variant="h4">Hi, Welcome back</Typography>
           <br/>
           <Home />
         </Box>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { getCourses, getCurrentCourses, getCurrentCoursesByID } from "src/api/co
 import { Course } from "src/sections/Course";
 import { Assignment } from '../sections/Assignment'
 import { getUser } from '../api/user';
-import { CardHeader, Grid } from '@mui/material';
+import { CardHeader, Grid, Typography, Paper } from '@mui/material';
 import { Box } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -16,75 +16,103 @@ import { UserContext } from "src/layouts/dashboard";
 import {useContext} from 'react';
 import { getUpcomingAssignments } from "src/api/AssignmentApi";
 
-export const Home = () =>{
+export const Home = () => {
     const [ user, setUser ] = useState(undefined);
 
     const userContext = useContext(UserContext);
     const navigate = useNavigate();
-
-    const [ missingAssignments, setMissingAssignments ] = useState([]);
     const [ currentCourses, setCurrentCourses ] = useState([]);
     const [ upcomingAssignments, setUpcomingAssignments ] = useState([]);
        
     useEffect(() => {
         getCurrentCoursesByID(userContext.user.user_id).then(x => setCurrentCourses(x));
-        getUpcomingAssignments().then(x => setUpcomingAssignments(x));
+        getUpcomingAssignments().then(x => setUpcomingAssignments(x.sort((a,b) => new Date(a.assignment_due_date) - new Date(b.assignment_due_date))));
     }, []);
 
     console.log(upcomingAssignments);
 
+    function formatDate (dateStr) {
+        const newdate = new Date (dateStr);
+        const options = { weekday: 'long', month: 'long', day: 'numeric'};
+        return newdate.toLocaleDateString('en-US', options);
+    }
     
-    
-    // if(user){
-    return<>
-        <Grid container spacing={2}>
-            <Grid item width={{xs: '100%', md: '80%'}}>
-                <MissingAssignments />
-            </Grid>
-            
-                <Grid item width={{xs: '50%'}} sx={{mt: 4}}>
-                    <h1>Current Courses: </h1>
-                    <h3 style={{color: 'GrayText'}}>Current Semester</h3>
-                    <div style={{padding: 5}}>
-                        {currentCourses.map((course, index) => 
-                            <Card key={index} sx={{mb: 2}}>
-                                <Box sx={{fontSize: 'large', pl:2, pt:1}}>{course.course_name}</Box>
-                                <CardActions>
-                                    <Button type="button" variant="text"
-                                        onClick={() => {
-                                            navigate(`courses/${course.couse_id}`);
-                                        }}>
-                                        Go to {course.course_name}'s course page
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        )}
-                    </div>
-                </Grid>
-                
-                <Grid item width={{xs:'50%'}} sx={{mt:4}}>
-                    <h1>Upcoming Assignments:</h1>
-                    {upcomingAssignments ?  
-                    upcomingAssignments.map((assignment, index) => (
-                        <Card key={index} sx={{mb:2}}>
-                            <CardContent>
-                                <Box fontWeight='bold'>{assignment.assignment_name}
-                                    <span style={{color: 'GrayText', float: 'right', marginRight: 2}} > Due Date: {assignment.dueDate}</span>
-                                </Box>
-                                <CardActions>
-                                    <Button>Go to {assignment.assignment_name}</Button>
-                                </CardActions>
-                            </CardContent>
-                            
-                        </Card>
-                    ))  :  <></>}
-                    
-                </Grid>
-                
-        </Grid>
-    </>
+    function currSemester () {
+        const date = new Date();
+        if(date.getMonth() < 5)
+         return "Spring " + date.getFullYear();
+        else
+            return "Fall " + date.getFullYear();
+    }
 
-    // else{
-    //     return<>Loading...</>
-    // }
+
+
+    return<>
+        <Typography variant="h4" style={{textAlign: 'center'}}>Hi, Welcome back {userContext.user.first_name}</Typography>
+            <br/>
+            <Paper elevation={3} alignItems={'center'}>
+                <Grid item width={{xs: '100%'}}>
+                    <MissingAssignments />
+                </Grid>
+            </Paper>
+            <br/>
+            <Paper elevation={3} alignItems={'center'} sx={{p:2}}>
+                <Grid container spacing={2}>
+                    <Grid item width={{xs: '50%'}}>
+                        <h1>Current Courses: </h1>
+                        <h3 style={{color: 'GrayText'}}>{currSemester()}</h3>
+                        <br/>
+                        <div >
+                            {currentCourses.map((course, index) => 
+                            <Paper elevation={10}>
+                                <Card key={index} sx={{mb: 2, padding:1}}>
+                                    <Box sx={{fontSize: 'large', pt:2, pl:2}} style={{fontWeight: 'bold'}}>
+                                        {course.course_name}
+                                        <span style={{fontSize: 15, color: 'GrayText', float: 'right', marginRight: '2vw'}}>
+                                            Professor: {course.professor_name}
+                                        </span>
+                                    </Box>
+                                    <CardActions>
+                                        <Button type="button" variant="text"
+                                            onClick={() => {
+                                                navigate(`courses/${course.couse_id}`);
+                                            }}>
+                                            Go to {course.course_name}'s course page
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Paper>
+                            )}
+                        </div>
+                    </Grid>
+        
+                    <Grid item width={{xs:'50%'}}>
+                        <div>
+                            <h1>Upcoming Assignments:</h1>
+                            <h3 style={{color: 'GrayText'}} >Assignments due in the next week</h3>
+                        </div>
+                        <br/>
+                        <div syle={{marginTop: 3}}>
+                            {upcomingAssignments ?  
+                            upcomingAssignments.map((assignment, index) => (
+                                <Paper elevation={10}>
+                                    <Card key={index} sx={{mb:2}}>
+                                        <CardContent>
+                                            <Box fontWeight='bold'>{assignment.assignment_name}
+                                                <span style={{color: 'GrayText', float: 'right', marginRight: 2}} > 
+                                                    Due Date: {formatDate(assignment.assignment_due_date)}
+                                                </span>
+                                                <br/>
+                                                <p  style={{color: 'GrayText', padding: 2}}>{assignment.assignment_description}</p>
+                                            </Box>
+                                        </CardContent>
+                                        
+                                    </Card>
+                                </Paper>
+                            ))  :  <></>}
+                        </div>  
+                    </Grid>
+                </Grid>
+            </Paper>                   
+    </>
 }
