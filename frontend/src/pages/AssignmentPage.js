ddadbb18d288adbc7c9af48bb34319b2c5c2463c
amandavmonaco <avmonaco@smu.edu>
2023-04-24 22:32:04 -0500

    assignment button

--- frontend/src/pages/AssignmentPage.js
+++ frontend/src/pages/AssignmentPage.js
@@ -1,20 +1,29 @@
-import { useState, useEffect, useNavigate, useParams } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { Box, Card, CardContent, Paper } from '@mui/material';
+
 import { getAssignmentById } from "src/api/AssignmentApi";
 
-
 export const AssignmentPage = () => {
-    const [ assignment, setAssignment ] = useState(null);
-    const navigate = useNavigate();
+    const [ assignment, setAssignment ] = useState();
     const params = useParams();
-
+    
 
     useEffect(() => {
-        getAssignmentById(params.asignment_id).then(x => setAssignment(x))
+        getAssignmentById(params.assignment_id).then(x => setAssignment(x));
+        console.log(params);
     }, []);
+
+    function formatDate (dateStr) {
+        const newdate = new Date (dateStr);
+        const options = { weekday: 'long', month: 'long', day: 'numeric'};
+        return newdate.toLocaleDateString('en-US', options);
+    }
 
     return <>
         <Paper elevation={10}>
-            <Card key={index} sx={{mb:2}}>
+            <Card sx={{mb:2}}>
                 <CardContent display='flex'>
                     <Box fontWeight='bold'>{assignment.assignment_name}
                         <span style={{color: 'GrayText', float: 'right', marginRight: 2}} > 
